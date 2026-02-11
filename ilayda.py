1bd888d4fde08fc12302af2931214c400159e278
Ilayda Sokur <ilaydasokur0@gmail.com>
2026-02-11 18:41:05 +0300

    Clean repo, keep library project only

--- ilayda.py
+++
@@ -1,6 +0,0 @@
-email="ilaydasokur0@gmail.com"
-şifre="ilayda123"
-if email==str(input("email address: ")) and şifre==str(input("password: ")):
-    print("giriş başarılı")
-else:
-    print("giriş başarısız")
